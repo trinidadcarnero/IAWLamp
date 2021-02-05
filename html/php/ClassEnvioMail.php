4890670050785f0b1cbee800dbf55ef046e1c48e
angelcorral <77585295+angelcorral@users.noreply.github.com>
2021-02-05 18:30:43 +0100

    Add files via upload

---
+++ html/php/ClassEnvioMail.php
@@ -0,0 +1,82 @@
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
+
+    require './vendor/phpmailer/phpmailer/src/PHPMailer.php';
+    require './vendor/phpmailer/phpmailer/src/Exception.php';
+    require './vendor/phpmailer/phpmailer/src/SMTP.php';
+    require './vendor/autoload.php';
+
+    class envioEmail
+    {
+        private $nombre;
+        private $email;
+
+
+        function __construct($nombre, $email) {
+            $this->nombre = $nombre;
+            $this->email = $email;
+        }
+
+
+
+
+        function sendMail() {
+            //Crear un objeto nuevo email
+            $mail = new PHPMailer();
+
+            //Usamos SMTP
+            $mail->isSMTP();
+
+            //Asignamos el servidor
+            $mail->Host = 'smtp.gmail.com';
+            // usar
+            // $mail->Host = gethostbyname('smtp.gmail.com');
+            // Si tu red no soporta ipV6
+
+            //Asignar el número de puerto SMTP  - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+            $mail->Port = 587;
+
+            //Asignar el mecanismo de encriptación - STARTTLS or SMTPS
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+            //Activamos comunicación segura SMTP authentication
+            $mail->SMTPAuth = true;
+
+            //Usuario que se logea en gmail - hay que usar la misma dirección de email completa
+            $mail->Username = 'angelpupa00@gmail.com';
+
+            //Contraseña de gmail para la SMTP authentication
+            $mail->Password = 'Akalitalonmidanlu3434';
+
+            //Asignar el 'desde'
+            $mail->setFrom("angelpupa00@gmail.com", "Angel");
+
+            // reply-to address
+            $mail->addReplyTo("angelpupa00@gmail.com", 'Tienda Web 3.0');
+
+            //Dirección de envío
+
+            $mail->addAddress("$this->email", "$this->nombre");
+
+            //Ponemos el asunto
+            $mail->Subject = 'Aviso de Insercion en Tienda Web 3.0';
+
+            //Read an HTML message body from an external file, convert referenced images to embedded,
+            //convert HTML into a basic plain-text alternative body
+            //$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+            $mail->msgHTML("<h3>Aviso</h3><br><p>Mensaje generado automaticamente.</p> <p>Le informamos de que el ".date(DATE_RFC2822)." fue dado de alta en Tienda Web 3.0 </p>", __DIR__);
+            //Replace the plain text body with one created manually
+            $mail->AltBody = 'This is a plain-text message body';
+
+            //Attach an image file
+            //$mail->addAttachment('images/phpmailer_mini.png');
+
+            //send the message, check for errors
+            $mail->send();
+        }
+
+    }
+?>
--- html/php/insertarClientes.php
+++ html/php/insertarClientes.php
@@ -1,29 +1,33 @@
 <?php
-$servername = "localhost";
-$username = "php";
-$password = "1234";
+  require 'ClassCliente.php';
+  require 'ClassEnvioMail.php';
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+  $servername = "localhost";
+  $database = "pruebas";
+  $username = "php";
+  $password = "1234";
 
-// Insertar Cliente
-$nombre= $_POST["nombre"];
-$apellidos= $_POST["apellido"];
-$dni= $_POST["dni"];
-$email= $_POST["email"];
-$nacimiento= $_POST["naci"];
+  $nombre=$_POST["nombre"];
+  $apellidos=$_POST["apellido"];
+  $dni=$_POST["dni"];
+  $email=$_POST["email"];
+  $fecha_nacimiento=$_POST["fecha_nacimiento"];
 
-$sql ="insert into pruebas.clientes (nombre,apellidos,dni,email,fecha_nacimiento) values ('$nombre','$apellidos','$dni','$email','$nacimiento')";
 
-if (mysqli_query($conn, $sql)==1) {
-    echo "Insertado Correctamente";
-} else {
-    echo "Error Insertando: " . mysqli_error($conn);
-}
 
-mysqli_close($conn);
+  // Se crea la conexion
+  $conn = mysqli_connect($servername, $username, $password, $database);
+  // Se comprueba la conexion
+  if (!$conn) {
+    die("ERROR CONECTANDO: " . mysqli_connect_error());
+  }
+
+
+  $clienteNuevo=new Cliente($nombre,$apellidos,$dni,$email,$fecha_nacimiento);
+  $clienteNuevo->darAlta($conn);
+
+  $miEmail = new envioEmail($nombre, $email);
+  $miEmail->sendMail($conn);
+  $conn-> close();
+
 ?>
